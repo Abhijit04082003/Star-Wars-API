aad3692908cba05c07358e52ed54d3151743256a
Abhijit04082003 <abhijitrajeev0407@gmail.com>
2024-01-22 11:01:17 +0530

    first commit

---
+++ src/Components/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Card = ({name = 'unknown', gender = 'n/a', weight = 'unknown', species = 'unknown' }) => {
+    
+    return (
+        <div className='tc bg-moon-gray dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img src={`https://robohash.org/${species}2`} alt='species'/> {/* Added the '2' after the species variable in-case there is no species and hence an empty array */}
+            <div>
+                <h2> {name} </h2>
+                <p> Gender: {gender} </p>
+                <p> Weight: {weight} </p>
+            </div>
+        </div>
+    );
+}
+ 
+export default Card
---
+++ src/Components/CardList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+// import { people } from '../people'; // redundant because we destructure it below in the "const Cardlist"
+import Card from './Card';
+
+const CardList = ({ people }) => {
+    return (
+        <div>
+            {
+                people.map((user, i) => { // second part of a map is always the index (i)
+                    return ( 
+                        <Card 
+                        key={people[i].url || '1'} 
+                        species={ people[i].species || 'unknown'} 
+                        name={people[i].name || 'Adam'} 
+                        gender={people[i].gender || 'Male'} 
+                        weight={people[i].mass || 'Too Much'}
+                        />
+                        );
+                    })
+}
+            
+        </div>
+    );
+    
+}
+
+// Card.defaultProps = {
+//     key: '1', 
+//     species: '1', 
+//     name: 'Adam',
+//     gender: 'Male',  
+//     weight: 'Too Much'
+// }
+
+export default CardList;
